a1a4b1a254e098234402421a566a0db8a0d87075
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-11-26 22:50:04 +0200

    Add spare parts and manufacturer tables.

---
+++ MaintSoft.Infrastructure/Data/Manufacturer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintSoft.Infrastructure.Data
+{
+    public class Manufacturer
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 7)]
+        public string VAT { get; set; } = null!;
+
+        [Required]
+        [StringLength(200, MinimumLength = 5)]
+        public string Contacts { get; set; } = null!;
+
+        [Required]
+        [StringLength(200, MinimumLength = 10)]
+        public string Address { get; set; } = null!;
+
+ 
+        [StringLength(200, MinimumLength = 0)]
+        public string Description { get; set; }
+
+        public List<SparePart> SpareParts { get; set; }
+
+    }
+}
---
+++ MaintSoft.Infrastructure/Data/SparePart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintSoft.Infrastructure.Data
+{
+    public class SparePart
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(100, MinimumLength = 3)]
+        public string Code { get; set; } = null!;
+
+        [Required]
+        [Range(0, 100000)]
+        public int Quantity { get; set; } = 0;
+
+
+        [StringLength(1000, MinimumLength = 5)]
+        public string Description { get; set; } = null!;
+
+
+        [StringLength(20, MinimumLength = 5)]
+        public string Location { get; set; } = null!;
+
+        [Required]
+        public int ManufacturerId { get; set; }
+
+        [ForeignKey(nameof(ManufacturerId))]
+        public Manufacturer Manufacturer { get; set; } = null!;
+    }
+}
